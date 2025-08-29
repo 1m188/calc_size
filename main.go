5de9f20c88558f69d0fe0ee9c137998b26362bb1
1m188 <228117330@qq.com>
2025-08-29 11:08:01 +0800

    添加命令行帮助信息功能

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 
 func main() {
 
+	args := os.Args
+
+	// 输出帮助信息
+	if len(args) == 2 && (args[1] == "--help" || args[1] == "--h" || args[1] == "-help" || args[1] == "-h") {
+
+		fmt.Println(`输入文件名或文件夹名称来计算文件或文件夹大小: 
+	calc_size [Your file name or folder name]
+
+或者是连续计算多个文件或者文件夹的大小: 
+	calc_size [file1] [file2] [file3] ...`)
+
+		return
+	}
+
 	file_name := "go.mod"
 
 	file_info, err := os.Stat(file_name)
