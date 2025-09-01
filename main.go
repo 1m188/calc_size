3111c85afc9aff6e1b46455c94ca21aa765fdd38
1m188 <228117330@qq.com>
2025-09-01 14:45:36 +0800

    提取帮助信息到独立文件

--- main.go
+++ main.go
@@ -7,14 +7,6 @@ import (
 	"strings"
 )
 
-var help_info string = `输入文件名或文件夹名称来计算文件或文件夹大小: 
-	calc_size [Your file name or folder name]
-
-或者是连续计算多个文件或者文件夹的大小: 
-	calc_size [file1] [file2] [file3] ...
-
-输出-1则表示该文件不存在或文件夹存在路径错误或者其他错误`
-
 // 输出文件大小
 func print_size(file_paths []string, file_sizes []int64) {
 
