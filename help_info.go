b57a9ca7df0bd6e5eebebc4c4c52675cea2f2d48
1m188 <228117330@qq.com>
2025-09-24 10:45:04 +0800

    更新帮助信息，调整命令行参数格式

--- help_info.go
+++ help_info.go
@@ -2,9 +2,9 @@ package main
 
 // 帮助信息
 var helpInfo string = `输入文件名或文件夹名称来计算文件或文件夹大小: 
-	calc_size [Your file name or folder name]
+	calc_size -p [Your file name or folder name]
 
 或者是连续计算多个文件或者文件夹的大小: 
-	calc_size [file1] [file2] [file3] ...
+	calc_size -p [file1],[file2],[file3] ...
 
 输出-1则表示该文件不存在或文件夹存在路径错误或者其他错误`
