b1a25c156a73c9c2343835ec4ee1a183fb66f501
1m188 <228117330@qq.com>
2025-09-29 14:35:19 +0800

    完善文件大小测试用例，添加异常场景测试

--- get_file_size/get_file_size_test.go
+++ get_file_size/get_file_size_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 )
 
+/* 测试 getFileSize 基本函数功能 */
 func TestGetFileSize(t *testing.T) {
 	sizes := []int64{0, 10 + 1, 1024 + 12, 1024*1024*2 + 123, 1024*1024*1024 + 1234}
 	var sum big.Int
@@ -34,9 +35,9 @@ func TestGetFileSize(t *testing.T) {
 		}
 		file.Close()
 
-		fileSizes := GetFilesSize([]string{file_name})
-		if fileSizes[0].Size.Cmp(big.NewInt(sizes[i])) != 0 {
-			t.Errorf("Expected %d, got %s", sizes[i], fileSizes[0].Size.String())
+		fileSize := getFileSize(file_name)
+		if fileSize.Size.Cmp(big.NewInt(sizes[i])) != 0 {
+			t.Errorf("Expected %d, got %s", sizes[i], fileSize.Size.String())
 		}
 
 		os.Remove(file_name)
@@ -67,9 +68,15 @@ func TestGetFileSize(t *testing.T) {
 		}
 		file.Close()
 	}
-	fileSizes := GetFilesSize([]string{".test"})
-	if fileSizes[0].Size.Cmp(&sum) != 0 {
-		t.Errorf("Expected %s, got %s", sum.String(), fileSizes[0].Size.String())
+	fileSize := getFileSize(".test")
+	if fileSize.Size.Cmp(&sum) != 0 {
+		t.Errorf("Expected %s, got %s", sum.String(), fileSize.Size.String())
 	}
 	os.RemoveAll(".test")
+
+	// 测试不存在的文件或文件夹
+	fileSize = getFileSize("./path/not/exist/file")
+	if fileSize.Size.Cmp(big.NewInt(-1)) != 0 {
+		t.Errorf("Expected -1, got %s", fileSize.Size.String())
+	}
 }
