565d18cf56ba8be86cd5faab81555c6ddf0b74df
1m188 <228117330@qq.com>
2025-09-30 11:00:59 +0800

    重构测试代码，提取文件创建逻辑到独立函数

--- get_file_size/get_file_size_test.go
+++ get_file_size/get_file_size_test.go
@@ -49,22 +49,10 @@ func TestGetFileSize(t *testing.T) {
 	// 测试文件大小
 	for i := range len(sizes) {
 		file_name := fmt.Sprintf(".test%d", i)
-		file, err := os.Create(file_name)
+		err := makeFile(file_name, sizes[i])
 		if err != nil {
 			t.Fatal(err)
 		}
-		for b := int64(0); b < sizes[i]; {
-			var block int64 = 1024 * 1024 * 1024
-			if sizes[i]-b < block {
-				block = sizes[i] - b
-			}
-			_, err := file.Write(make([]byte, block))
-			if err != nil {
-				t.Fatal(err)
-			}
-			b += block
-		}
-		file.Close()
 
 		fileSize := getFileSize(file_name)
 		if fileSize.Size.Cmp(big.NewInt(sizes[i])) != 0 {
@@ -75,29 +63,12 @@ func TestGetFileSize(t *testing.T) {
 	}
 
 	// 测试文件夹大小
-	err := os.Mkdir(".test", 0755)
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	for i := range len(sizes) {
 		file_name := fmt.Sprintf("%s/.test%d", ".test", i)
-		file, err := os.Create(file_name)
+		err := makeFile(file_name, sizes[i])
 		if err != nil {
 			t.Fatal(err)
 		}
-		for b := int64(0); b < sizes[i]; {
-			var block int64 = 1024 * 1024 * 1024
-			if sizes[i]-b < block {
-				block = sizes[i] - b
-			}
-			_, err := file.Write(make([]byte, block))
-			if err != nil {
-				t.Fatal(err)
-			}
-			b += block
-		}
-		file.Close()
 	}
 	fileSize := getFileSize(".test")
 	if fileSize.Size.Cmp(&sum) != 0 {
